de409574c0a20feb70c01aefbd09ac9cf793ac92
Artur Osypenko <artur.osypenko@dev-pro.net>
2021-03-13 16:15:57 +0200

    Add basic 2d canvas image and install canvas-sketch

--- main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const canvasElement = document.getElementById("canvas");
-  const ctx = canvasElement.getContext("2d");
-
-  ctx.fillStyle = "#2ec4b6";
-  ctx.fillRect(10, 10, 150, 100);
-});
